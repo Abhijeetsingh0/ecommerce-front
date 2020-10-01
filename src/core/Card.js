202464ba51c92c2a80db2282372c35056cea9df4
Abhijeet singh <anish16253481@gmail.com>
2020-10-01 22:50:58 +0530

    fix bug

--- src/core/Card.js
+++ src/core/Card.js
@@ -72,7 +72,7 @@ const Card = ({product ,
 
       const handleChange = productId => event => {
         setRun(!run); // run useEffect in parent Cart
-        setCount(event.target.value < 1 ? 1 : event.target.value);
+        setCount(event.target.value < 1 ? 0 : event.target.value);
         if (event.target.value >= 1) {
           updateItem(productId, event.target.value);
         }
